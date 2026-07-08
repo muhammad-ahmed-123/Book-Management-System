4bb941ba7744c9191272e8f237ff7a7dd05820da
muhammad-ahmed-123 <muhammad.ahmed@techieminions.co>
2026-07-08 11:40:38 +0500

    Removed some redundant code from rpsec

--- spec/models/book_genre_spec.rb
+++ spec/models/book_genre_spec.rb
@@ -5,20 +5,6 @@ RSpec.describe BookGenre, type: :model do
   let(:genre) { Genre.create!(name: "Fiction") }
   let(:book) { Book.create!(title: "A Title", author: "An Author", user: user, genres: [ genre ]) }
 
-  describe "validations" do
-    it "is invalid without a book" do
-      book_genre = BookGenre.new(book: nil, genre: genre)
-
-      expect(book_genre).not_to be_valid
-    end
-
-    it "is invalid without a genre" do
-      book_genre = BookGenre.new(book: book, genre: nil)
-
-      expect(book_genre).not_to be_valid
-    end
-  end
-
   describe "uniqueness of the (book, genre) pair" do
     it "raises at the database level when the same pair is inserted twice" do
       expect {
